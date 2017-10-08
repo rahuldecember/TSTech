c93f1eb4d6771db2d47f74e50c08d86baec3041b
Rahul <rahuldecember@gmail.com>
2017-10-08 16:00:00 +0100

    Response code condition added

--- src/utility/FlickrApiResponse.java
+++ src/utility/FlickrApiResponse.java
@@ -3,7 +3,6 @@ package utility;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -13,14 +12,14 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-
 public class FlickrApiResponse {
 
-	HashMap<Integer,String>  al = new HashMap<Integer,String> ();
+	HashMap<Integer, String> al = new HashMap<Integer, String>();
 
-	public HashMap<Integer,String>  GetResponse(String searchText) throws ClientProtocolException, IOException {
+	public HashMap<Integer, String> GetResponse(String searchText) throws ClientProtocolException, IOException {
 
-		String url = "https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1&tags=" + searchText;
+		String url = "https://api.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1&tags="
+				+ searchText;
 
 		URL obj = new URL(url);
 		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
@@ -28,45 +27,48 @@ public class FlickrApiResponse {
 		// optional default is GET
 		con.setRequestMethod("GET");
 
+		//http Connection connect
 		con.connect();
 
+		//fetching response code of request.
 		int responseCode = con.getResponseCode();
 		System.out.println("\nSending 'GET' request to URL : " + url);
 		System.out.println("Response Code : " + responseCode);
 
-		String inline = "";
-		Scanner sc = new Scanner(obj.openStream());
+		// if responsecode is 200 then json response will be parsed.
+		if (responseCode == 200) {
+			String inline = "";
+			Scanner sc = new Scanner(obj.openStream());
 
-		while (sc.hasNext()) {
-			inline += sc.nextLine();
-		}
-
-		JSONParser parser = new JSONParser();
-
-		// try {
-
-		Object object;
-		try {
-			object = parser.parse(inline);
-
-			// convert Object to JSONObject
-			JSONObject jsonObject = (JSONObject) object;
-
-			// loop array
-			JSONArray msg = (JSONArray) jsonObject.get("items");
-
-			for (int i = 0; i < msg.size(); i++) {
-				JSONObject rec = (JSONObject) msg.get(i);
-				String title = (String) rec.get("title");
-				al.put(i,title);
+			while (sc.hasNext()) {
+				inline += sc.nextLine();
 			}
 
-		} catch (ParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+			JSONParser parser = new JSONParser();
 
-		sc.close();
+			Object object;
+			try {
+				object = parser.parse(inline);
+
+				// convert Object to JSONObject
+				JSONObject jsonObject = (JSONObject) object;
+
+				// loop array
+				JSONArray msg = (JSONArray) jsonObject.get("items");
+
+				for (int i = 0; i < msg.size(); i++) {
+					JSONObject rec = (JSONObject) msg.get(i);
+					String title = (String) rec.get("title");
+					al.put(i, title);
+				}
+
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			sc.close();
+		}
 
 		return al;
 
